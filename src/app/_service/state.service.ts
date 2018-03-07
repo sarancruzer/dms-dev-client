640f0786b6c82442b0e7e76a7862168fd6a761aa
Saravanan Nandhan <sarancruzer@gmail.com>
2018-03-07 15:04:35 +0530

    code commit

--- src/app/_service/state.service.ts
+++ src/app/_service/state.service.ts
@@ -1,9 +1,10 @@
+import { environment } from 'environments/environment';
 import { Injectable } from '@angular/core';
-import { environment } from "environments/environment";
 import { State } from "app/_model/state";
 import { Http, RequestOptions, Response ,Headers} from '@angular/http';
 import 'rxjs/Rx';
 import { Observable } from "rxjs/Observable";
+
 
 @Injectable()
 export class StateService {
